20a1f586fdedd0161cb57a8a1df69099c08df56b
adejes <adejes@gmail.com>
2013-11-17 13:02:51 +0100

    Initial release

---
+++ gen/es/jcyl/cs/mdoco/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package es.jcyl.cs.mdoco;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
