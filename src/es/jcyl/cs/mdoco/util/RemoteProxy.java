c712d8a0e3923a77e8d23be3d1597a0a52b53a54
halfbeat <adejes@gmail.com>
2014-04-01 23:35:57 +0200

    --amend

--- src/es/jcyl/cs/mdoco/util/RemoteProxy.java
+++ src/es/jcyl/cs/mdoco/util/RemoteProxy.java
@@ -1,8 +1,8 @@
 package es.jcyl.cs.mdoco.util;
 
 public class RemoteProxy {
-    protected static final String SERVER_NAME = "192.168.0.13";
+    protected static final String SERVER_NAME = "192.168.0.11";
     protected static final int SERVER_PORT = 9090;
-    protected static final String REST_SERVICE_BASE = "http://" + SERVER_NAME + ":" + SERVER_PORT;
+    protected static final String REST_SERVICE_BASE = "http://" + SERVER_NAME + ":" + SERVER_PORT + "/rest";
 
 }
